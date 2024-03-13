2c956c8b35e5b21d9c3eb09b8c865045420a7347
Matodev11 <msmojvir1@gmail.com>
2024-03-13 16:46:32 +0100

    added 404 not found

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Layout from "./components/layout";
 import Countries from "./components/Countries";
 import Home from "./components/home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "./components/notfound";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/countries" element={<Countries/>} />
+          <Route path="*" element={<NotFound/>} />
           
         </Routes>
       </Layout>
